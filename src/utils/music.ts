3582eda8899ce28fc36bc15b42558f517fc7ab5c
EvolutionX <jeyush2005@gmail.com>
2022-10-30 21:45:14 +0530

    fix: ffmpeg not found error

--- src/utils/music.ts
+++ src/utils/music.ts
@@ -2,6 +2,7 @@ import { logger } from '#lib/structures';
 import { Presets, SingleBar } from 'cli-progress';
 import { blueBright, greenBright, red, underline, yellowBright } from 'colorette';
 import ffmpeg from 'fluent-ffmpeg';
+import ffmpegPath from 'ffmpeg-static';
 import { writeFile } from 'node:fs/promises';
 import { tmpdir } from 'node:os';
 import { join } from 'node:path';
@@ -25,6 +26,8 @@ export function map(videos: Video[]) {
 }
 
 export async function save(song: Video, overrideformat?: 'mp3' | 'flac', metadata?: SpotifySong) {
+	ffmpeg.setFfmpegPath(ffmpegPath!);
+
 	const { bitrate, format } = getConfig(true);
 	overrideformat ??= format;
 
