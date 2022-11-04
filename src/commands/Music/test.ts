808fd645b1249af8e03fe88088fd29284e7c1ba3
EvolutionX <jeyush2005@gmail.com>
2022-11-04 08:19:51 +0530

    chore: if it's hacky and you know it clap you hands (clap clap)!
    permalink: http://whatthecommit.com/cc3fb5d2aa5eb2c21ea707df706689ae

--- src/commands/Music/test.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from '#lib/structures';
-import { getAudioFeatures, searchSpotify } from '#utils';
-
-export default new Command({
-	async run() {
-		const e = await searchSpotify('colors halsey');
-		console.log(await getAudioFeatures(e.tracks.items[0].id));
-	}
-});
